54ee11b04e9ff6c95d522da1c332052a446b3b63
mhd-zam <111276559+mhd-zam@users.noreply.github.com>
2023-07-08 17:39:32 +0530

    Update GraphChart.jsx

--- admin/src/component/GraphChart.jsx
+++ admin/src/component/GraphChart.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Line } from 'react-chartjs-2'
+/* eslint-disable no-unused-vars */
 import {Chart as ChartJS} from 'chart.js/auto'
 function GraphChart({chartData}) {
   return (

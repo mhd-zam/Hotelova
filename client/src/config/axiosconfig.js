f9003b2236d13cc0da07ef3a2baf55e3ae483d54
mhd-zam <111276559+mhd-zam@users.noreply.github.com>
2023-07-08 23:37:22 +0530

    Update axiosconfig.js

--- client/src/config/axiosconfig.js
+++ client/src/config/axiosconfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Axios = axios.create({
-    baseURL: 'https://www.hotelova.site/server/users',
+    baseURL: 'http://www.hotelova.site/server/users',
     withCredentials: true,
 })
 export default Axios

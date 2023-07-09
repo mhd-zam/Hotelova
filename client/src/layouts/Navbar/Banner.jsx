ddf025b77de842f400c0fa96480d2c343283e8a2
Mohammed Zamil <111276559+mhd-zam@users.noreply.github.com>
2023-07-09 15:44:00 +0530

    Update Banner.jsx

--- client/src/layouts/Navbar/Banner.jsx
+++ client/src/layouts/Navbar/Banner.jsx
@@ -85,7 +85,7 @@ function Banner() {
                 alignItems: 'center',
                 p: 2,
                 backgroundImage:
-                    'url("https://hotelova.s3.ap-south-1.amazonaws.com/7041389_437.jpg")',
+                    'url("https://hotelova.s3.ap-south-1.amazonaws.com/wp1846099.jpg")',
                 backgroundSize: 'cover',
                 backgroundPosition: 'center',
                 backgroundRepeat: 'no-repeat',

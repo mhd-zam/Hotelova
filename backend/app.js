fff819a67db7d2f943c6bc8270ceac6f04d3d6d3
Mohammed Zamil <111276559+mhd-zam@users.noreply.github.com>
2023-07-09 16:22:06 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const http = require("http");
 const server = http.createServer(app);
 const io = require("socket.io")(server,{path:'/server/socket.io/'},{
   cors: {
-    origin: "https://hotelova.site",
+    origin: "https://hotelova.site/",
     methods:["GET","POST","OPTIONS"]
   },
 });
